5eac6b4270b622087a27a82545d2208e5a4c6a76
Mario Eugenio <mgomes@gvdasa.com.br>
2023-07-27 12:46:31 -0300

    fix: readme

--- src/Poc.Cliente.Publish/Program.cs
+++ src/Poc.Cliente.Publish/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IProducerService, ProducerService>();
 
 ConfigurationManager Configuration = builder.Configuration;
 
@@ -21,18 +20,6 @@ if (app.Environment.IsDevelopment())
 }
 
 var origins = Configuration["OrigensCORS"].Split(";");
-
-builder.Services.AddCors(options =>
-    {
-        options.AddPolicy(name: MyAllowSpecificOrigins,
-            builder =>
-            {
-                builder.WithOrigins(origins)
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials();
-            });
-    });
 
 app.UseHttpsRedirection();
 
